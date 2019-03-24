3297ab437a3b729fa8c38ec0cffd231490109c7d
ahmad_dr1998 <ahmedrarga@gmail.com>
2019-03-24 17:52:34 +0200

    Database updated, sign in and sign up changed

--- app/src/main/java/com/example/securityprojectofficial/MainActivity.java
+++ app/src/main/java/com/example/securityprojectofficial/MainActivity.java
@@ -20,7 +20,6 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth ;
 
 
     @Override
@@ -28,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(MainActivity.this);
-        mAuth = FirebaseAuth.getInstance();
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             Intent intent = new Intent(MainActivity.this, Blind.class);
             startActivity(intent);
@@ -38,77 +36,23 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-
-    public void registerButton(View v) {
-        Intent intent = new Intent(MainActivity.this, Register.class);
+    public void blindButton(View view){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("userType", "blindUser");
+        intent.putExtras(bundle);
         startActivity(intent);
-
     }
+    public void friendButton(View view){
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        bundle.putString("userType", "friendUser");
+        intent.putExtras(bundle);
+        intent = new Intent(MainActivity.this, LoginActivity.class);
 
-    public void loginButton(View v) {
-
-        final EditText userEmail = (EditText) findViewById(R.id.email);
-        final EditText password = (EditText) findViewById(R.id.password);
-        final String sUserEmail = userEmail.getText().toString();
-        final String sPassword = password.getText().toString();
-        userEmail.setError(null);
-        password.setError(null);
-        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-        progressDialog.setTitle("loging...");
-        progressDialog.setMessage("Please wait.");
-        if (sUserEmail.isEmpty())
-            userEmail.setError("Username is empty!");
-        if (sPassword.isEmpty())
-            password.setError("Password is empty!");
-
-        mAuth.signInWithEmailAndPassword(sUserEmail, sPassword)
-                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("jj", "signInWithEmail:success");
-                            Toast.makeText(MainActivity.this, "login success.",
-                                    Toast.LENGTH_SHORT).show();
-                            progressDialog.dismiss();
-                            login();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            try {
-                                throw task.getException();
-                            } catch (FirebaseAuthWeakPasswordException e) {
-                                progressDialog.dismiss();
-                                password.setError("Invalid password!");
-                                password.requestFocus();
-                            } catch (FirebaseAuthInvalidCredentialsException e) {
-                                progressDialog.dismiss();
-                                password.setError("Invalid password!");
-                                password.requestFocus();
-                            } catch (FirebaseAuthUserCollisionException e) {
-                                progressDialog.dismiss();
-                                Log.e("EMAIL", e.getMessage());
-                            } catch (Exception e) {
-                                progressDialog.dismiss();
-                                Log.e("", e.getMessage());
-                            }
-
-
-                        }
-
-                        // ...
-                    }
-                });
-    }
-
-
-    private void login() {
-        Intent intent = new Intent(MainActivity.this, Blind.class);
         startActivity(intent);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        finish();
-
     }
+
 }
 
 
--- app/src/main/java/com/example/securityprojectofficial/Register.java
+++ app/src/main/java/com/example/securityprojectofficial/Register.java
@@ -11,95 +11,108 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    private String userType;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Bundle b = getIntent().getExtras();
+        userType = b.getString("userType");
     }
-    public void regButton(View v) {
-        final EditText userEmail = (EditText) findViewById(R.id.emailR);
-        final EditText password = (EditText) findViewById(R.id.passwordR);
-        final EditText repassword = (EditText) findViewById(R.id.repasswordR);
-        final String sUserEmail = userEmail.getText().toString();
-        final String sPassword = password.getText().toString();
-        final String sRePassword = password.getText().toString();
 
-        userEmail.setError(null);
-        password.setError(null);
-        repassword.setError(null);
+    public void regButton(View v) {
+        final EditText fName = ((EditText) findViewById(R.id.fName));
+        final EditText lName = ((EditText) findViewById(R.id.lName));
+        final EditText userPNum = ((EditText) findViewById(R.id.pNum));
+        final EditText pass = ((EditText) findViewById(R.id.pass));
+        final EditText rePass = ((EditText) findViewById(R.id.rePass));
+
+        String sFName = fName.getText().toString();
+        String sLName = lName.getText().toString();
+        String SUserPNum = userPNum.getText().toString();
+        String SPass = pass.getText().toString();
+        String SRePass = rePass.getText().toString();
+        fName.setError(null);
+        lName.setError(null);
+        userPNum.setError(null);
+        pass.setError(null);
+        rePass.setError(null);
         final ProgressDialog progressDialog = new ProgressDialog(Register.this);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         progressDialog.setTitle("registering...");
         progressDialog.setMessage("Please wait.");
-        if (sUserEmail.isEmpty()) {
-            userEmail.setError("Username is empty!");
-            userEmail.requestFocus();
+        if (sFName.isEmpty()) {
+            userPNum.setError("First Name is empty!");
+            userPNum.requestFocus();
         }
-        if (sPassword.isEmpty()) {
-            password.setError("Password is empty!");
-            password.requestFocus();
+        if (sLName.isEmpty()) {
+            userPNum.setError("Last Name is empty!");
+            userPNum.requestFocus();
         }
-        if(sRePassword.isEmpty()) {
-            repassword.setError("re-Password is empty!");
-            repassword.requestFocus();
+        if (SUserPNum.isEmpty()) {
+            userPNum.setError("Username is empty!");
+            userPNum.requestFocus();
         }
-        final FirebaseAuth mAuth= FirebaseAuth.getInstance();;
+        if (SPass.isEmpty()) {
+            pass.setError("Password is empty!");
+            pass.requestFocus();
+        }
+        if (SRePass.isEmpty()) {
+            rePass.setError("re-Password is empty!");
+            rePass.requestFocus();
+        }
 
-        if(!sUserEmail.isEmpty() && !sPassword.isEmpty() &&!sRePassword.isEmpty() && sPassword.equals(sRePassword) ) {
+        Map<String, String> user = new HashMap<>();
+        user.put("fName", sFName);
+        user.put("lName", sLName);
+        user.put("pNum", SUserPNum);
+        user.put("pass", SPass);
+
+
+        if (SPass.equals(SRePass)) {
             progressDialog.show();
-            mAuth.createUserWithEmailAndPassword(sUserEmail, sPassword).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        progressDialog.dismiss();
-                        Toast.makeText(Register.this, "Authentication success.",
-                                Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(Register.this, MainActivity.class);
-                        startActivity(intent);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        finish();
-
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        try {
-                            throw task.getException();
-                        } catch (FirebaseAuthWeakPasswordException e) {
-                            password.setError("Password must be at least 6 characters!");
-                            password.requestFocus();
+            db.collection(userType)
+                    .add(user)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
                             progressDialog.dismiss();
-                        } catch (FirebaseAuthInvalidCredentialsException e) {
-                            userEmail.setError("Email address is badly formatted!");
-                            userEmail.requestFocus();
-                            progressDialog.dismiss();
-                        } catch (FirebaseAuthUserCollisionException e) {
-                            userEmail.setError("Email address is exist!");
-                            userEmail.requestFocus();
-                            progressDialog.dismiss();
-                        } catch (Exception e) {
+                            Toast.makeText(Register.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Register.this, Blind.class);
+                            startActivity(intent);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
                             Log.e("", e.getMessage());
                             progressDialog.dismiss();
                         }
-                    }
-                }
-
-             });
+                    });
         }
-        }
-
-
-
-
-
-
     }
+}
 
 
